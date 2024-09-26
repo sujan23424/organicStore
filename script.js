7bab06d81c06c7c426b8fbec9ca1b7e3408c1977
sujan23424 <sujankoinbox@gmail.com>
2024-09-26 19:59:26 +0545

    let const var

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 //     document.querySelector("nav div ul").style.display = "block";
 // }
 
-function shownavbar(){
+function shownavbar() {
     var list = document.querySelector(".mobile");
     list.classList.toggle("list");
     list.classList.toggle("showdesign");
@@ -31,4 +31,35 @@ function shownavbar(){
 //     });
 //   });
 
+// var show = document.getElementById("date");
+// function showdate() {
+//     var date = new Date();
+//     var hours = date.getHours();
+//     var minutes = date.getMinutes();
+//     var seconds = date.getSeconds();
+//     show.innerHTML = hours + ":" + minutes + ":" + seconds;
+// }
+// setInterval(showdate, 1000);
 
+// var a = 5;
+// var b = 100;
+
+// var m;
+// m = 499;
+// m = 1000;
+// console.log(a);
+// console.log(b);
+// show();
+// function show(){
+//     console.log("Hello world");
+// }
+
+// let a;
+// a = 100;
+// a = 300;
+// console.log(a);
+// const b = 100;
+// console.log(100);
+
+// const n = 3.14;
+// console.log(n);
